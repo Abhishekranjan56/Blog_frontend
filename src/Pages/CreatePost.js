5de5d3d0a91f683def42bfbbfebd977591cc7767
Abhishek Ranjan <abhishekranjan@Abhisheks-MacBook-Air.local>
2023-06-29 22:13:30 +0530

    first commit

---
+++ src/Pages/CreatePost.js
@@ -0,0 +1,49 @@
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+import {useState} from "react";
+import {Navigate} from "react-router-dom";
+import Editor from "../Editor";
+
+export default function CreatePost() {
+  const [title,setTitle] = useState('');
+  const [summary,setSummary] = useState('');
+  const [content,setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  async function createNewPost(ev) {
+    const datas = new FormData();
+    datas.set('title', title);
+    datas.set('summary', summary);
+    datas.set('content', content);
+    datas.set('file', files[0]);
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'POST',
+      body: datas,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+  return (
+    <form onSubmit={createNewPost}>
+      <input type="title"
+             placeholder={'Title'}
+             value={title}
+             onChange={ev => setTitle(ev.target.value)} />
+      <input type="summary"
+             placeholder={'Summary'}
+             value={summary}
+             onChange={ev => setSummary(ev.target.value)} />
+      <input type="file"
+             onChange={ev => setFiles(ev.target.files)} />
+      <Editor value={content} onChange={setContent} />
+      <button style={{marginTop:'5px'}}>Create post</button>
+    </form>
+  );
+}
